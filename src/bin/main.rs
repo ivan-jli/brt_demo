ed4c7bf7c3dcb97b7ad71b96d4893609952d2f18
Ivan Ivanov <ivan@home>
2025-08-08 16:04:44 +0100

    wip, unable to put the uC in sleep and wake-up on GPIO change. Interesting though, when     accel_int_in.wakeup_enable(true, WakeEvent::LowLevel).unwrap();  it doesn't wake at all when I press the button, and when instead of Low it's High, it stays awake (I guess it wakes immediately after sleep).

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -11,19 +11,22 @@
 use esp_backtrace as _;
 use cassette::Cassette;
 use esp_hal::{
-    delay::Delay, gpio::{Event, Input, InputConfig, Io, Pull}, handler, i2c::master::*, main, rtc_cntl::{
-        reset_reason, sleep::{GpioWakeupSource, TimerWakeupSource, WakeSource, WakeupLevel}, wakeup_cause, Rtc, Rwdt, RwdtStage, SocResetReason
+    delay::Delay, gpio::{Event, Input, InputConfig, Io, Pull, WakeEvent}, handler, i2c::master::*, main, rtc_cntl::{
+        reset_reason, sleep::{GpioWakeupSource, RtcSleepConfig, TimerWakeupSource, WakeSource, WakeTriggers, WakeupLevel}, wakeup_cause, Rtc, Rwdt, RwdtStage, SocResetReason
         
     }, system::Cpu, time::Duration, timer::{timg::TimerGroup, PeriodicTimer, Timer}, DriverMode
 };
 use esp_println::println;
 use critical_section::Mutex;
 use core::cell::RefCell;
-use lis3dh_async::{Lis3dh, SlaveAddr};
+use lis3dh_async::{Lis3dh, SlaveAddr, IrqPin1Config, Interrupt1, InterruptConfig, InterruptMode};
 
 static ACCEL_INT: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
 //RWDT usage example from esp_hal/rtc_cntl
 static RWDT: Mutex<RefCell<Option<Rwdt>>> = Mutex::new(RefCell::new(None));
+static HALL_INT: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
+
+static STATE: Mutex<RefCell<State>> = Mutex::new(RefCell::new(State::Sleeping));
 
 esp_bootloader_esp_idf::esp_app_desc!();
 
@@ -34,8 +37,8 @@ fn main() -> ! {
     let mut io = Io::new(peripherals.IO_MUX);
     io.set_interrupt_handler(gpio_handler);
 
-    // i2c
-    let mut i2c = I2c::new(peripherals.I2C0, Config::default()).unwrap().into_async()
+    // Setting i2c
+    let i2c = I2c::new(peripherals.I2C0, Config::default()).unwrap().into_async()
     .with_sda(peripherals.GPIO1)
     .with_scl(peripherals.GPIO2);
     
@@ -67,44 +70,68 @@ fn main() -> ! {
         .with_pull(Pull::Up), 
     );
     
+    // Setting accelerometer
+    // let x = core::pin::pin!(accel_init(i2c));
+    // let cm = Cassette::new(x);
+    // cm.block_on();
 
     println!("Init successful.");
     let reason = reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
     println!("Reset reason: {:?}", reason);
 
-    // Call the entry point future, and pin it
-    let x = core::pin::pin!(accel_init(i2c));
-    // Give the pinned future to Cassette for execution
-    let mut cm = Cassette::new(x);
-
-
-    // let delay = Delay::new();
+    let delay = Delay::new();
     // let timer = TimerWakeupSource::new(CoreDuration::from_secs(10));
 
     // core::mem::drop(accel_int_in);
 
-    // let rtcio = GpioWakeupSource::default();  //:: ???????
-    accel_int_in.wakeup_enable(true, esp_hal::gpio::WakeEvent::HighLevel).unwrap();
-    // rtcio.apply(&rtc, triggers, sleep_config); //todo?
+    let mut rtc_lpwr = Rtc::new(peripherals.LPWR);
+    // let gpio_wakeup_source = GpioWakeupSource::default();
+    let gpio_wakeup_source = GpioWakeupSource::new();
+    
+    let mut wake_triggers = WakeTriggers::default();
+    wake_triggers.set_gpio(true);
 
-    // println!("going to light sleep!");
-    // delay.delay_millis(100);
-    // rtc.sleep_light(&[&rtcio]);
+    let mut sleep_config = RtcSleepConfig::default();
+    // sleep_config.wifi_pd_en()
+
+    gpio_wakeup_source.apply(&rtc_lpwr, &mut wake_triggers, &mut sleep_config);
+
+    // gpio_wakeup_source.apply(&rtc_lpwr, sleep_config).wakeup_enable(true, esp_hal::gpio::WakeEvent::HighLevel).unwrap(); // wakeup_enable unlistens for interrupts
+    // gpio_wakeup_source.apply(&rtc_lpwr, triggers, sleep_config); //todo?
+
     
     let wake_reason = wakeup_cause();
     println!("wake reason: {:?}", wake_reason);
     
-    critical_section::with(|cs| {
-        accel_int_in.listen(Event::LowLevel);
-        ACCEL_INT.borrow_ref_mut(cs).replace(accel_int_in);
-    });
+    // commenting out accelerometer init in order to test on esp hardware without that sensor
+    // critical_section::with(|cs| {
+    //     accel_int_in.listen(Event::LowLevel);
+    //     ACCEL_INT.borrow_ref_mut(cs).replace(accel_int_in);
+    // });
 
+    accel_int_in.wakeup_enable(true, WakeEvent::LowLevel).unwrap();
+    // accel_int_in.wakeup_enable(true, WakeEvent::HighLevel).unwrap();
+    println!("Going to light sleep!");
+    delay.delay_millis(100);
+    rtc_lpwr.sleep_light(&[&gpio_wakeup_source]); // 250810 2003 gpio_wakeup_source isn't correct
+    delay.delay_millis(100);
+    println!("exiting light sleep!");
+    
     let mut counter: u32 = 0;
+    // let mut state = State::default();
     loop {
         counter += 1;
         if counter % 50_000_000 == 0 {
             println!(".");
         }
+
+    // critical_section::with(|cs| {
+    //     let mut a = HALL_INT.borrow_ref_mut(cs);
+    //     let Some(a) = a.as_mut() else {
+    //         return;
+    //     };
+    //     // if a.
+    // });
         
     }
 }
@@ -154,7 +181,7 @@ fn gpio_handler() {
 
 
 #[handler]
-fn rtc_handler() {
+fn rtc_handler() { //periodic interrupt each x ms
     critical_section::with(|cs| {
         println!("rtc ISR");
         let mut a = RWDT.borrow_ref_mut(cs);
@@ -166,7 +193,46 @@ fn rtc_handler() {
     });
 }
 
+#[handler]
+fn gpio_hall_sensor_handler() {
+    critical_section::with(|cs| {
+        let mut a = HALL_INT.borrow_ref_mut(cs);
+        let Some(a) = a.as_mut() else {
+            return;
+        };
+        a.clear_interrupt();
+        let mut state = STATE.borrow_ref_mut(cs);
+        *state = State::HallSensorStateChangeIndication;
 
-async fn accel_init<'a, Dm: DriverMode>(i2c: I2c<'a, Dm>) where esp_hal::i2c::master::I2c<'a, Dm>: embedded_hal_async::i2c::I2c {
-    let mut lis3dh = Lis3dh::new_i2c(i2c, SlaveAddr::Alternate).await.unwrap();
+    });
 }
+
+
+async fn accel_init<'a, Dm: DriverMode>(i2c: I2c<'a, Dm>) 
+// -> Lis3dh<lis3dh_async::Lis3dhI2C<I2c<'a, Dm>>>
+where esp_hal::i2c::master::I2c<'a, Dm>: embedded_hal_async::i2c::I2c {
+    let mut lis3dh = Lis3dh::new_i2c(i2c, SlaveAddr::Alternate).await.unwrap();
+    lis3dh.configure_interrupt_pin(IrqPin1Config {
+        // Raise if interrupt 1 is raised
+        ia1_en: true,
+        // Disable for all other interrupts
+        ..IrqPin1Config::default()
+    }).await.unwrap();
+    lis3dh.configure_irq_src(Interrupt1, InterruptMode::Movement, 
+        InterruptConfig::high() 
+        // { z_axis_high: (), z_axis_low: (), y_axis_high: (), y_axis_low: (), x_axis_high: (), x_axis_low: () }
+    ).await.unwrap();
+}
+
+enum State {
+    Sleeping,
+    Movement,
+    Movement10sIndication,
+    HallSensorStateChangeIndication,
+}
+
+// impl Default for State {
+//     fn default() -> Self {
+//         State::Sleeping
+//     }
+// }
