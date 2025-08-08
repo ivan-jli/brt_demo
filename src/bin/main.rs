6de9c710c26eca4a98de3c87ab45e966170cbf9c
Ivan Ivanov <ivan@home>
2025-08-08 09:41:35 +0100

    32316f4 (HEAD -> master, public) compiles f577444 wip, broken. Add async fn accel_init bb6c5ff add Cassette async env 413a47f rm unnecessary deps - embassy-* b0df799 working on Lis3dh. Err: multiple versions of embedded_hal 50e4b0e still continuous rtc ISR :/ 373cdd0 rtc ISR fired (continuously) 7825efa wip, builds. Setting Timer0 1594c23 implementing timer-based interrupts, still doesn't work 9eed49b add rtc_handler and RWDT a159d06 add Movement struct ff24a0b it compiles, but the wakeup isn't confgured properly yet, so the device stays in sleep 58418c1 fixing the borrow error by removing .reborrow() . TODO check why it was needed 78a1511 wip, still broken, implementing the example from esp_hal listen(). Borrow checker problems 8654ace compiles! 03d3ea5 main fixed 429f505 wip, added wakeup interrupt, complains that there's no main 65eba03 wip, broken but much better 6c074e5 builds

---
+++ src/bin/main.rs
@@ -0,0 +1,172 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+// use core::time::Duration;
+
+use esp_backtrace as _;
+use cassette::Cassette;
+use esp_hal::{
+    delay::Delay, gpio::{Event, Input, InputConfig, Io, Pull}, handler, i2c::master::*, main, rtc_cntl::{
+        reset_reason, sleep::{GpioWakeupSource, TimerWakeupSource, WakeSource, WakeupLevel}, wakeup_cause, Rtc, Rwdt, RwdtStage, SocResetReason
+        
+    }, system::Cpu, time::Duration, timer::{timg::TimerGroup, PeriodicTimer, Timer}, DriverMode
+};
+use esp_println::println;
+use critical_section::Mutex;
+use core::cell::RefCell;
+use lis3dh_async::{Lis3dh, SlaveAddr};
+
+static ACCEL_INT: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
+//RWDT usage example from esp_hal/rtc_cntl
+static RWDT: Mutex<RefCell<Option<Rwdt>>> = Mutex::new(RefCell::new(None));
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+    // Setting ISR handlers
+    let mut io = Io::new(peripherals.IO_MUX);
+    io.set_interrupt_handler(gpio_handler);
+
+    // i2c
+    let mut i2c = I2c::new(peripherals.I2C0, Config::default()).unwrap().into_async()
+    .with_sda(peripherals.GPIO1)
+    .with_scl(peripherals.GPIO2);
+    
+    // Setting Timer
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    let mut timer0 = PeriodicTimer::new(timg0.timer0);
+    // timer0.start(Duration::from_secs(1)).unwrap();
+    // let now = timer0.now();
+    // timer0.load_value(Duration::from_secs(1)).unwrap();
+    timer0.set_interrupt_handler(rtc_handler);
+    timer0.enable_interrupt(true);
+    // timer0.start();
+    
+    // Setting RWDT
+    // timg0.rwdt.set_timeout(RwdtStage::Stage0, Duration::from_millis(10));
+    // timg0.rwdt.listen();
+    // critical_section::with(|cs| RWDT.borrow_ref_mut(cs).replace(timer0));
+    
+    // Where the `LP_WDT` interrupt handler is defined as:
+    // static RWDT: Mutex<RefCell<Option<Rwdt>>> = Mutex::new(RefCell::new(None));
+
+    // Setting GPIO
+    let accel_int = peripherals.GPIO9; //GPIO9 == BOOT Button
+    let pin3 = peripherals.GPIO3;
+    let mut accel_int_in = Input::new(
+        accel_int,
+        InputConfig::default()
+        // .with_pull(Pull::Down), //LIS3DH 38/54 DocID17530 Rev 2: default INT_POLARITY 0 - active high
+        .with_pull(Pull::Up), 
+    );
+    
+
+    println!("Init successful.");
+    let reason = reset_reason(Cpu::ProCpu).unwrap_or(SocResetReason::ChipPowerOn);
+    println!("Reset reason: {:?}", reason);
+
+    // Call the entry point future, and pin it
+    let x = core::pin::pin!(accel_init(i2c));
+    // Give the pinned future to Cassette for execution
+    let mut cm = Cassette::new(x);
+
+
+    // let delay = Delay::new();
+    // let timer = TimerWakeupSource::new(CoreDuration::from_secs(10));
+
+    // core::mem::drop(accel_int_in);
+
+    // let rtcio = GpioWakeupSource::default();  //:: ???????
+    accel_int_in.wakeup_enable(true, esp_hal::gpio::WakeEvent::HighLevel).unwrap();
+    // rtcio.apply(&rtc, triggers, sleep_config); //todo?
+
+    // println!("going to light sleep!");
+    // delay.delay_millis(100);
+    // rtc.sleep_light(&[&rtcio]);
+    
+    let wake_reason = wakeup_cause();
+    println!("wake reason: {:?}", wake_reason);
+    
+    critical_section::with(|cs| {
+        accel_int_in.listen(Event::LowLevel);
+        ACCEL_INT.borrow_ref_mut(cs).replace(accel_int_in);
+    });
+
+    let mut counter: u32 = 0;
+    loop {
+        counter += 1;
+        if counter % 50_000_000 == 0 {
+            println!(".");
+        }
+        
+    }
+}
+
+struct Movement {
+    moving: u32,
+    still: u32,
+}
+
+impl Movement {
+    fn new() -> Self {
+        Movement {
+            moving: 0,
+            still: 0,
+        }
+    }
+    fn add_move(&mut self) {
+        self.moving += 1;
+    }
+    fn add_still(&mut self) {
+        self.still += 1;
+    }
+    fn is_moving(self) -> bool {
+        self.moving > 0 && self.moving > self.still
+    }
+}
+
+//using the example from esp_hal::gpio::Input listen()
+#[handler]
+fn gpio_handler() {
+    critical_section::with(|cs| {
+        let mut accel_int = ACCEL_INT.borrow_ref_mut(cs);
+        let Some(accel_int) = accel_int.as_mut() else {
+            // Some other interrupt has occurred
+            // before the button was set up.
+            return;
+        };
+
+        if accel_int.is_interrupt_set() {
+            accel_int.clear_interrupt();
+            println!("A ISR!");
+            // accel_int.unlisten();
+            // loop {};
+        }
+    });
+}
+
+
+#[handler]
+fn rtc_handler() {
+    critical_section::with(|cs| {
+        println!("rtc ISR");
+        let mut a = RWDT.borrow_ref_mut(cs);
+        let Some(a) = a.as_mut() else {
+            return;
+        };
+        a.clear_interrupt();
+        // a.unlisten();
+    });
+}
+
+
+async fn accel_init<'a, Dm: DriverMode>(i2c: I2c<'a, Dm>) where esp_hal::i2c::master::I2c<'a, Dm>: embedded_hal_async::i2c::I2c {
+    let mut lis3dh = Lis3dh::new_i2c(i2c, SlaveAddr::Alternate).await.unwrap();
+}
